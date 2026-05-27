76cb32122d04fe0d61ffe5503e922bdeb01d17d2
Luiz Gustavo Sabadim Spolon Junqueira <luizgustavossj@gmail.com>
2026-05-27 17:37:06 -0400

    feat: configurable read interval

--- cmd/client/main.go
+++ cmd/client/main.go
@@ -5,6 +5,7 @@ import (
 	"log"
 	"os"
 	"os/signal"
+	"strconv"
 	"syscall"
 	"time"
 
@@ -14,16 +15,25 @@ import (
 )
 
 func main() {
-	godotenv.Load()
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("could not load .env file: ", err)
+	}
 
-	var token, agentID, addr string
+	var token, agentID, addr, secondsInterval string
 
 	token = os.Getenv("HOMELENS_AUTH_TOKEN")
 	agentID = os.Getenv("HOMELENS_AGENT_ID")
 	addr = os.Getenv("HOMELENS_SERVER_ADDR")
+	secondsInterval = os.Getenv("HOMELENS_SECONDS_INTERVAL")
 
 	if token == "" || agentID == "" || addr == "" {
 		log.Fatal("HOMELENS_AUTH_TOKEN, HOMELENS_AGENT_ID, and HOMELENS_SERVER_ADDR environment variables must be set")
+	}
+
+	interval, err := strconv.Atoi(secondsInterval)
+	if err != nil {
+		interval = 10
 	}
 
 	ctx, cancel := context.WithCancel(context.Background())
@@ -34,7 +44,7 @@ func main() {
 
 	agentClient := client.NewAgentClient(log.Printf, token, agentID, addr)
 
-	if err := agentClient.Run(ctx, time.Second*5); err != nil {
+	if err := agentClient.Run(ctx, time.Second*time.Duration(interval)); err != nil {
 		log.Fatalf("agent client error: %v", err)
 	}
 }
