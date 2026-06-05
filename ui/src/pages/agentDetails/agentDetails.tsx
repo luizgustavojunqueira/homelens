10c8f6e5d633eb7f8e3d10ea6c3bd8978638a9c3
Luiz Gustavo Sabadim Spolon Junqueira <luizgustavossj@gmail.com>
2026-06-05 15:49:41 -0400

    feat: add mem history chart

--- ui/src/pages/agentDetails/agentDetails.tsx
+++ ui/src/pages/agentDetails/agentDetails.tsx
@@ -19,6 +19,8 @@ export default function AgentDetails() {
     });
   }, [agentId]);
 
+  const timestamps = agent?.history.map((snap) => snap.timestamp) ?? [];
+
   const currentDiskUsage =
     agent?.latest_snapshot?.data.disk_space.usage_percent ?? 0;
   const currentDiskUsed = agent?.latest_snapshot?.data.disk_space.used ?? 0;
@@ -26,13 +28,11 @@ export default function AgentDetails() {
   const diskUsedHistory = agent?.history
     .map((snap) => convertByteToMetric(snap.data.disk_space.used, "GB"))
     .filter((du) => du !== undefined);
-  const diskUsedTimestamps = agent?.history.map((snap) => snap.timestamp);
 
   const currentCpuAvgUsage = agent?.latest_snapshot.data.cpu_usage.cpu_avg ?? 0;
   const cpuAvgHistory = agent?.history
     .map((snap) => snap.data.cpu_usage.cpu_avg)
     .filter((cu) => cu !== undefined);
-  const cpuAvgTimestamps = agent?.history.map((snap) => snap.timestamp);
 
   const cpusHistory = agent?.history[0].data.cpu_usage.cpu_info.map(
     (_, index) =>
@@ -45,11 +45,15 @@ export default function AgentDetails() {
   const currentMemTotal = agent?.latest_snapshot.data.memory.total ?? 0;
   const currentMemUsage = (currentMemUsed / currentMemTotal) * 100;
 
+  const memUsedHistory = agent?.history.map((snap) =>
+    convertByteToMetric(snap.data.memory.used, "GB", "KB"),
+  );
+
   return (
     <div className="px-6 py-6 flex-1 overflow-y-auto">
       <h2 className="text-lg font-medium text-(--text) mb-4">{agent?.name}</h2>
 
-      <div className="mb-6 flex flex-row justify- gap-6">
+      <div className="mb-6 flex flex-row justify-center gap-6">
         <div className="w-full">
           <DiskGauge
             value={currentDiskUsage}
@@ -80,7 +84,7 @@ export default function AgentDetails() {
           <Line
             values={diskUsedHistory ?? []}
             valueFormatter={(value: number) => `${value.toFixed(2)} GB`}
-            timestamps={diskUsedTimestamps ?? []}
+            timestamps={timestamps}
             label="Disk Used (GB)"
           />
         </div>
@@ -90,9 +94,18 @@ export default function AgentDetails() {
             values={cpuAvgHistory ?? []}
             valueFormatter={(value: number) => `${value.toFixed(2)} %`}
             secondaryValues={cpusHistory}
-            timestamps={cpuAvgTimestamps ?? []}
+            timestamps={timestamps}
             label="CPU Average Usage (%)"
             tooltipItemPrefix="CPU"
+          />
+        </div>
+
+        <div className="w-full">
+          <Line
+            values={memUsedHistory ?? []}
+            valueFormatter={(value: number) => formatByteStr(value, "GB")}
+            timestamps={timestamps}
+            label="RAM Used (GB)"
           />
         </div>
       </div>
--- ui/src/utils/index.ts
+++ ui/src/utils/index.ts
@@ -1,9 +1,10 @@
-
 type Metric = "B" | "KB" | "MB" | "GB" | "TB" | "PB" | "EB" | "ZB" | "YB";
 
 const metrics: Metric[] = ["B", "KB", "MB", "GB", "TB", "PB", "EB", "ZB", "YB"];
-export function formatByteStr(value: number, currentMetric: Metric = "B"): string {
-
+export function formatByteStr(
+  value: number,
+  currentMetric: Metric = "B",
+): string {
   if (value === 0) {
     return `${value.toFixed(2)} ${currentMetric}`;
   }
@@ -13,18 +14,31 @@ export function formatByteStr(value: number, currentMetric: Metric = "B"): strin
   const factor = Math.pow(1024, metricIndex);
   const valueInBytes = value * factor;
 
-  const newMetricIndex = Math.floor(Math.log(valueInBytes) / Math.log(1024))
-  const newValue = valueInBytes / Math.pow(1024, newMetricIndex)
+  const newMetricIndex = Math.floor(Math.log(valueInBytes) / Math.log(1024));
+  const newValue = valueInBytes / Math.pow(1024, newMetricIndex);
 
   return `${newValue.toFixed(2)} ${metrics[newMetricIndex]}`;
 }
 
-export function convertByteToMetric(bytes: number, targetMetric: Metric): number {
+export function convertByteToMetric(
+  value: number,
+  targetMetric: Metric,
+  currentMetric: Metric = "B",
+): number {
+  if (value === 0) {
+    return value;
+  }
+
+  const currentMetricIndex = metrics.indexOf(currentMetric);
+
+  const factorCurrent = Math.pow(1024, currentMetricIndex);
+  const valueInBytes = value * factorCurrent;
+
   const targetIndex = metrics.indexOf(targetMetric);
   if (targetIndex === -1) {
     throw new Error(`Invalid target metric: ${targetMetric}`);
   }
-  const factor = Math.pow(1024, targetIndex);
-  return bytes / factor;
-}
 
+  const factor = Math.pow(1024, targetIndex);
+  return valueInBytes / factor;
+}
